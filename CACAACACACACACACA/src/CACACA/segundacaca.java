8b2f1f172814688d97819aa2e595b3e37f062aad
natalia <nmartinez5146@ceuandalucia.es>
2023-11-29 11:59:56 +0100

    sc

---
+++ CACAACACACACACACA/src/CACACA/segundacaca.java
@@ -0,0 +1,58 @@
+package CACACA;
+import java.util.Scanner;
+public class segundacaca {
+
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int i = 000000000;
+		int j = 0000000000000;
+		int k = 00000000000;
+
+		do {
+
+			System.out.println("Escriba un número mayor que 0");
+			i = scanner.nextInt();
+			
+
+		}
+
+		while (i <= 0);
+		scanner.close();
+		
+		for (j = 1; j <= i; j++) {
+			if(j==1) {
+				System.out.print(" __");
+			}
+			else {
+				System.out.print("___");
+			}
+		}
+		System.out.println();
+
+		for (j = 1; j <= i; j++) {
+			System.out.print("|");
+			System.out.print("__");
+
+			if (j == i) {
+				System.out.println("|");
+
+				for (k = 2; k <= i; k++) {
+
+					for (j = 1; j <= i; j++) {
+						System.out.print("|");
+						System.out.print("__");
+
+						if (j == i) {
+							System.out.println("|");
+						}
+					}
+
+				}
+
+			}
+
+		}
+		
+	}
+	}
